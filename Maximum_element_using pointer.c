b471c06eda7e754e710ae275d6316492686c45f3
Eshwari. A <eshwari.arumugam1@gmail.com>
2026-02-07 19:52:00 +0530

    Create Maximum_element_using pointer.c

---
+++ Maximum_element_using pointer.c
@@ -0,0 +1,15 @@
+//print maximum using pointer
+#include <stdio.h>
+
+int main() {
+    int a[6]={2,3,4,5,6,7};
+    
+    int *max=a;
+    for (int i=1;i<6;i++)
+    {
+       if(*(a+i)>*max) 
+       max=(a+i);
+       }
+     printf("%d ", *max);
+    return 0;
+}
