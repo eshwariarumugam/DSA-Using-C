eb2c3490d859dbcdf09a3f823c71d80109e678f1
Eshwari. A <eshwari.arumugam1@gmail.com>
2026-02-07 19:08:54 +0530

    Create Array_traversal.c

---
+++ Array_traversal.c
@@ -0,0 +1,12 @@
+//array traversal 
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {10, 20, 30, 40, 50};
+
+    for(int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
+//Time complexity:O(n)
