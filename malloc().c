1eaca63839c81d78c570e571e5dec9751aa30618
Eshwari. A <eshwari.arumugam1@gmail.com>
2026-02-10 19:14:29 +0530

    Create malloc().c

---
+++ malloc().c
@@ -0,0 +1,21 @@
+//malloc()
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+int n;
+scanf("%d",&n);
+int*a=(int*)malloc(n*sizeof(int));
+if (a==NULL){
+    printf("Failed");
+    return 0;}
+for (int i=0;i<n;i++){
+    a[i]=i+1;}
+for (int i=0;i<n;i++){
+     printf("%d \n",a[i]);
+     }
+ free(a);
+return 0;
+}      
+   
