2fc4c2b83cb727c97a8e90d36523a448f927324b
Eshwari. A <eshwari.arumugam1@gmail.com>
2026-02-14 19:36:27 +0530

    Create Create_linked_list_n_nodes.c

---
+++ Create_linked_list_n_nodes.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node* next;
+};
+
+int main()
+{
+    int n;
+    struct node *head=NULL, *newnode=NULL, *temp=NULL;
+
+    printf("Enter number of nodes: ");
+    scanf("%d",&n);
+
+    for (int i=0; i<n; i++){
+
+        newnode=(struct node*) malloc(sizeof(struct node));
+
+        printf("Enter data at node %d: ", i+1);
+        scanf("%d",&newnode->data);
+        newnode->next=NULL;
+
+        if(head==NULL){
+            head=newnode;
+            temp=newnode;
+        }
+        else{
+            temp->next=newnode;
+            temp=newnode;
+        }
+    }
+
+    temp=head;
+    printf("Linked list:\n");
+
+    while(temp!=NULL){
+        printf("%d -> ", temp->data);
+        temp=temp->next;
+    }
+
+    // Free memory properly
+    temp = head;
+    while(temp != NULL){
+        struct node *nextNode = temp->next;
+        free(temp);
+        temp = nextNode;
+    }
+
+    return 0;
+}
+//Time complexity:O(n)
